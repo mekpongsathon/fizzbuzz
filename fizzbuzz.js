2e7e8d0d1a828e6b5269154db2277098fd4d4428
mekpongsathon <Pongsathon.Rakthong@gmail.com>
2019-09-21 16:27:44 +0700

    add test (error)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,37 @@
 class FizzBuzz {
-    static say(number) {
-        return 1;
+    // static say(number) {
+    //     if (number % 3 === 0 && number % 5 === 0)
+    //         return "FizzBuzz";
+    //     else if (number % 3 === 0)
+    //         return "Fizz";
+    //     else if (number % 5 === 0)
+    //         return "Buzz";
+    //     else return number;
+    // }
+    // let b1 = (number % 3 === 0);
+    // let b2 = (number % 5 === 0);
+    static Say(number) {
+        // switch (true) {
+        //     case true && true:
+        //         return "FizzBuzz";
+        //         break;
+        //     case true && false:
+        //         return "Fizz";
+        //         break;
+        //     case false && true:
+        //         return "Buzz";
+        //         break;
+        //     default:
+        //         return number;
+        // }
+    }
+
+    static mod(number, base) {
+        while (number > base) {
+            nunber -= base;
+        }
+        return number;
+
     }
 }
 module.exports = FizzBuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -5,4 +5,24 @@ describe("Test FizzBuzz", () => {
         const result = FizzBuzz.say(1);
         expect(result).toEqual(1) // Assest function
     });
+
+    test("Test 1 expect 1", () => {
+        const result = FizzBuzz.say(3);
+        expect(result).toEqual("Fizz") // Assest function
+    });
+
+    test("Test 1 expect 1", () => {
+        const result = FizzBuzz.say(5);
+        expect(result).toEqual("Buzz") // Assest function
+    });
+
+    test("Test 1 expect 1", () => {
+        const result = FizzBuzz.say(15);
+        expect(result).toEqual("FizzBuzz") // Assest function
+    });
+
+    test("test5mod5 ruturn0", () => {
+
+    });
+
 });
